896e922553d1e83941db1c0af4755bf303a6028e
Dmitry Khenkin <teddy.khenkin@gmail.com>
2024-02-04 21:02:25 +0200

    v.3.1
    Get all data in JSON.

--- LINUX/SAP/check_SAP.sh
+++ LINUX/SAP/check_SAP.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 #   Check status of SAP services and application by SAPcontrol utility
-#	
-#	v.2.1
+#   use sapcontrol & saphostcontrol utilities
+#
+#	v.3.1
 # 	by D.Khenkin 2024
 #
-#	usage: check_SAP.sh discovery   			- create JSON for DDL zabbix
-#              check_SAP.sh ServiceDescription iCOD             - return status 
+#	usage: check_SAP.sh 	- create JSON for DDL zabbix & status
+#
 #
 
-case ${1} in
-discovery)
-	SAPCMD="/usr/sap/hostctrl/exe/saphostctrl"
-	INST=$(${SAPCMD} -function ListInstances | awk '{print $4" "$6}')
+    SAPCMD="/usr/sap/hostctrl/exe/saphostctrl"
+    INST=$(${SAPCMD} -function ListInstances | awk '{print $4" "$6}')
 
     echo "{"
     echo "\"data\":["
@@ -24,35 +23,23 @@ discovery)
             PROC=${PROC// /_}
             if [ -n ${#PROC[@]} ]; then
                 for i in $PROC
-					do
-						sNAME=$(printf "%s $i" | awk -F ',' '{print $1}' | tr -d ' ')
-						sDESC=$(printf "%s $i" | awk -F ',' '{print $2}' | tr -d ' ')
-						echo "    $comma{\"{#INAME}\":\"$iNAME\" ,"
-						echo "    \"{#ICOD}\":\"$iCOD\", "
-						echo "    \"{#SNAME}\":\"$sNAME\", "
-						echo "    \"{#SDESC}\":\"$sDESC\"} "
-						comma=","
-						#sSTATUS=$(printf "%s $i" | awk -F ',' '{print $4}' | tr -d ' ')
-						#echo "$sNAME $sDESC $sSTATUS"
-					done
+                    do
+                        pNAME=$(printf "%s $i" | awk -F ',' '{print $1}' | tr -d ' ')
+                        pDESC=$(printf "%s $i" | awk -F ',' '{print $2}' | tr -d ' ')
+			pSTAT=$(printf "%s $i" | awk -F ',' '{print $3}' | tr -d ' ')
+
+			echo "    $comma{\"{#INAME}\":\"$iNAME\" ,"
+                        echo "    \"{#ICOD}\":\"$iCOD\", "
+                        echo "    \"{#PNAME}\":\"$pNAME\", "
+                        echo "    \"{#PDESC}\":\"$pDESC\", "
+			echo "    \"INDEX\":\"${iCOD}${iNAME}${pNAME}\", "
+			echo "    \"STATUS\":\"$pSTAT\"} "
+                        comma=","
+                    done
             fi
         done << EOF
 $INST
 EOF
-        echo "]"
-        echo "}"
-;;
-*)
-	sDESC=$1
-	iCOD=$2
+    echo "]"
+    echo "}"
 
-	sDESC=${sDESC//_/\ }
-	sDESC=${sDESC//\"}", GREEN"
-
-	if ( /usr/sap/hostctrl/exe/sapcontrol -nr $iCOD -function GetProcessList | grep "$sDESC" 1>/dev/null 2>&1 ); then
-        echo 1
-	else
-        echo 0
-	fi
-;;
-esac
